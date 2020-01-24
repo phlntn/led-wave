8a4ff14f78cbd412c99a0ec68a9df6a31b31b82e
Philipp Antoni <phil@infinise.com>
2020-01-23 20:43:45 -0800

    stuff

--- led-wave.cc
+++ led-wave.cc
@@ -27,33 +27,41 @@ class GrayScaleBlock : public ThreadedCanvasManipulator {
 public:
   GrayScaleBlock(Canvas *m) : ThreadedCanvasManipulator(m) {}
   void Run() {
+
     const int sub_blocks = 16;
     const int width = canvas()->width();
     const int height = canvas()->height();
     const int x_step = max(1, width / sub_blocks);
     const int y_step = max(1, height / sub_blocks);
     uint8_t count = 0;
+
     while (running() && !interrupt_received) {
       for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-          int c = sub_blocks * (y / y_step) + x / x_step;
-          switch (count % 4) {
-          case 0: canvas()->SetPixel(x, y, c, c, c); break;
-          case 1: canvas()->SetPixel(x, y, c, 0, 0); break;
-          case 2: canvas()->SetPixel(x, y, 0, c, 0); break;
-          case 3: canvas()->SetPixel(x, y, 0, 0, c); break;
-          }
+
+          int c = (count + y) % 255;
+          canvas()->SetPixel(x, y, c, c, c);
+
+          // int c = sub_blocks * (y / y_step) + x / x_step;
+          // switch (count % 4) {
+          //   case 0: canvas()->SetPixel(x, y, c, c, c); break;
+          //   case 1: canvas()->SetPixel(x, y, c, 0, 0); break;
+          //   case 2: canvas()->SetPixel(x, y, 0, c, 0); break;
+          //   case 3: canvas()->SetPixel(x, y, 0, 0, c); break;
+          // }
+
         }
       }
       count++;
-      sleep(2);
+      sleep(1 / 60);
     }
+
   }
 };
 
 int main(int argc, char *argv[]) {
   RGBMatrix::Options defaults;
-  
+
   // These are the defaults when no command-line flags are given.
   defaults.hardware_mapping = "adafruit-hat-pwm";  // or e.g. "adafruit-hat"
   defaults.cols = 64;
